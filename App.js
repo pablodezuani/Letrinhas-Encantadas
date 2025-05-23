f5178c8b1a3c1d6e00f1384aafb2cae30ab8ec69
PAblo Dezuani <pablodezuani@gmail.com>
2025-05-23 18:26:52 -0300

    :tada: feat : Project Start .

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import ReadingGame from './screens/ReadingGame';  
+import VowelsGame from './screens/VowelsGame';  
+import WordFormationGame from './screens/WordFormationGame';
+import PhraseBuilder from './screens/PhraseBuilder';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }} 
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ReadingGame" component={ReadingGame} />
+        <Stack.Screen name="VowelsGame" component={VowelsGame} />
+        <Stack.Screen name="WordFormationGame" component={WordFormationGame} />
+<Stack.Screen name="PhraseBuilder" component={PhraseBuilder} />
+
+
+ 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ types/navigation.ts
@@ -0,0 +1,8 @@
+export type RootStackParamList = {
+  Home: { avatarId: number; gender: 'menino' | 'menina' };
+  Login: undefined;
+  ReadingGame: { gender: 'menino' | 'menina' };
+  PhraseBuilder: { gender: 'menino' | 'menina' };
+  VowelsGame: { gender: 'menino' | 'menina' };
+    WordFormationGame: { gender: 'menino' | 'menina' };
+};
